5d481961e2fa1bb6e3da9ac54da8c7e0a72eff98
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-01-16 17:06:19 +0330

    Dev user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+import UserRoleEnum from "../enums/userRole.enum";
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  phone_number: string;
+
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(35)
+  display_name: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: "enum", enum: UserRoleEnum, default: UserRoleEnum.NormalUser })
+  role: UserRoleEnum;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
---
+++ src/users/enums/userRole.enum.ts
@@ -0,0 +1,6 @@
+enum UserRoleEnum {
+  NormalUser = "user",
+  AdminUser = "admin",
+}
+
+export default UserRoleEnum;
