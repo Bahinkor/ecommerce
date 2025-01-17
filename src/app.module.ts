45dd929d0df2270612bab4f045e7a0571a48ba2d
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-01-17 14:33:05 +0330

    Dev auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { UsersModule } from "./users/users.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -25,6 +26,8 @@ import { UsersModule } from "./users/users.module";
 
     // modules
     UsersModule,
+
+    AuthModule,
   ],
 })
 export class AppModule {}
