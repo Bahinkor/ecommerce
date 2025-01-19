b2534ae0566f47f220e2ad950b0b56ec591f0f68
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-01-19 19:47:24 +0330

    Dev ticket dto

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTicketDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateTicketDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  subject: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  user_id: number;
+
+  @IsNumber()
+  @IsOptional()
+  replay_to: number;
+}
