b9c302208c70cec2e0c2aa086b609e23ae3a3351
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-01-19 21:51:03 +0330

    Dev create ticket service

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "src/users/users.module";
 
 import { Ticket } from "./entities/ticket.entity";
 import { TicketsController } from "./tickets.controller";
 import { TicketsService } from "./tickets.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket])],
+  imports: [TypeOrmModule.forFeature([Ticket]), UsersModule],
   controllers: [TicketsController],
   providers: [TicketsService],
 })
--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTicketDto } from './dto/create-ticket.dto';
-import { UpdateTicketDto } from './dto/update-ticket.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UsersService } from "src/users/users.service";
+import { Repository } from "typeorm";
+
+import { CreateTicketDto } from "./dto/create-ticket.dto";
+import { UpdateTicketDto } from "./dto/update-ticket.dto";
+import { Ticket } from "./entities/ticket.entity";
 
 @Injectable()
 export class TicketsService {
-  create(createTicketDto: CreateTicketDto) {
-    return 'This action adds a new ticket';
+  constructor(
+    @InjectRepository(Ticket)
+    private readonly ticketRepository: Repository<Ticket>,
+    private readonly userService: UsersService,
+  ) {}
+
+  async create(createTicketDto: CreateTicketDto): Promise<Ticket> {
+    try {
+      const { user_id, replay_to, ...ticketData } = createTicketDto;
+      const user = await this.userService.findOne(user_id);
+      const replayToTicket = await this.ticketRepository.findOneByOrFail({ id: replay_to });
+
+      const newTicket = this.ticketRepository.create({
+        ...ticketData,
+        user,
+        reply_to: replayToTicket,
+      });
+
+      return await this.ticketRepository.save(newTicket);
+    } catch (e) {
+      throw new NotFoundException(e.message);
+    }
   }
 
   findAll() {
