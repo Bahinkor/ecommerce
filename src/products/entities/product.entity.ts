dd31eff83f9d7baba1c1f6672250fc8ff0adf596
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-02-10 21:04:19 +0330

    Dev product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,34 @@
-export class Product {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("products")
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  stock: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  // @ManyToMany(() => Categories, (category) => category.product)
+}
