389a11639ddd9a14c6f9f06207f11f662b444b76
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-02-10 22:41:28 +0330

    Dev product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,9 @@
+import { Category } from "src/categories/entities/category.entity";
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinTable,
   ManyToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
@@ -30,5 +32,11 @@ export class Product {
   @UpdateDateColumn()
   updated_at: Date;
 
-  // @ManyToMany(() => Categories, (category) => category.product)
+  @ManyToMany(() => Category, (category: Category) => category.products)
+  @JoinTable({
+    name: "product_categories",
+    joinColumns: [{ name: "product_id", referencedColumnName: "id" }],
+    inverseJoinColumns: [{ name: "category_id", referencedColumnName: "id" }],
+  })
+  categories: Category[];
 }
