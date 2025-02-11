7048e5f42a9a1fc654575ce91c1e2cf18c5995c7
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-02-11 16:25:22 +0330

    Dev category controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, Post, Res } from "@nestjs/common";
+import { Response } from "express";
 
 import { CategoriesService } from "./categories.service";
 import { CreateCategoryDto } from "./dto/create-category.dto";
@@ -8,12 +9,24 @@ export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  async create(@Res() res: Response, @Body() createCategoryDto: CreateCategoryDto) {
+    const category = await this.categoriesService.create(createCategoryDto);
+
+    return res.status(HttpStatus.CREATED).json({
+      data: category,
+      statusCode: HttpStatus.CREATED,
+      message: "Category created successfully",
+    });
   }
 
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  async findAll(@Res() res: Response) {
+    const categories = await this.categoriesService.findAll();
+
+    return res.status(HttpStatus.OK).json({
+      data: categories,
+      statusCode: HttpStatus.OK,
+      message: "Categories fetched successfully",
+    });
   }
 }
