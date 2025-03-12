0a061503f3e28734d61988715d8c5e408b271191
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-03-12 20:47:36 +0330

    oops

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,7 +1,10 @@
 import type { TestingModule } from "@nestjs/testing";
 
 import { Test } from "@nestjs/testing";
+import { get } from "http";
+import { createQueryBuilder } from "typeorm";
 
+import { User } from "./entities/user.entity";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
@@ -9,11 +12,20 @@ describe("UsersService", () => {
   let usersController: UsersController;
   let usersService: UsersService;
 
+  const mockQueryBuilder = {
+    where: jest.fn().mockReturnThis(),
+    addSelect: jest.fn().mockReturnThis(),
+    skip: jest.fn().mockReturnThis(),
+    take: jest.fn().mockReturnThis(),
+    getMany: jest.fn().mockResolvedValue([User]),
+  };
+
   const mockUserRepository = {
     find: jest.fn(),
     findOne: jest.fn(),
     save: jest.fn(),
     remove: jest.fn(),
+    createQueryBuilder: jest.fn(() => mockQueryBuilder),
   };
 
   beforeEach(async () => {
@@ -28,5 +40,12 @@ describe("UsersService", () => {
 
   it("should be defined", () => {
     expect(usersService).toBeDefined();
+  });
+
+  it("calls UserService.findAll and returns the result", async () => {
+    const result: User[] = [new User()];
+
+    jest.spyOn(usersService, "findAll").mockResolvedValue(result);
+    await expect(usersService.findAll()).resolves.toEqual(result);
   });
 });
