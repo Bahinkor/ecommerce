1a06018df2bcefd8e3b1eae1369d31041c636625
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2025-04-07 11:51:55 +0330

    chore: specify NestExpressApplication type in bootstrap for enhanced IntelliSense

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import type { NestExpressApplication } from "@nestjs/platform-express";
+
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+  app.setGlobalPrefix("api");
+  app.enableVersioning();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
